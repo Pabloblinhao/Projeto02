d4bc8c82f031a55275355ba4ebfb3d54652ec44b
Pablo <pablo_vitor1707@hotmail.com>
2022-07-13 19:42:37 -0300

    first commit

---
+++ js/index.js
@@ -0,0 +1,13 @@
+var div = document.getElementById('text-skils');
+
+function changeHtml() {
+    div.innerHTML = "HTML (Linguagem de Marcação de Hipertexto) é o bloco de construção mais básico da web. Define o significado e a estrutura do conteúdo da web."
+}
+
+function changeCss() {
+    div.innerHTML = "CSS é uma linguagem de folha de estilo composta por 'camadas', criado com o propósito de estilizar as páginas HTML."
+}
+
+function changeJS() {
+    div.innerHTML = "JavaScript é uma linguagem de programação interpretada estruturada, de script em alto nível com tipagem dinâmica fraca e multiparadigma."
+}
